172d89001f49f3de3c2a4a53d25e70020ee74b6a
CypherForest <99782301+CypherForest@users.noreply.github.com>
2022-05-23 09:36:34 -0600

    Update hackmo.js

--- js/hackmo.js
+++ js/hackmo.js
@@ -20,22 +20,22 @@ image(hackmo,0,0);
 textSize(26)
 fill(200,200,20);
 setTimeout(denQ1,1000);
-setTimeout(denQ2,2000);
-setTimeout(denQ3,3000);
-setTimeout(denQ4,4000);
-setTimeout(detQ1,5000);
-setTimeout(detQ2,6000);
-setTimeout(detQ3,7000);
+setTimeout(detQ1,2000);
+setTimeout(denQ2,3000);
+setTimeout(detQ2,4000);
+setTimeout(denQ3,5000);
+setTimeout(detQ3,6000);
+setTimeout(denQ4,7000);
 setTimeout(detQ4,8000);
 setTimeout(reset,10000);
-setTimeout(denQ1,11000);
-setTimeout(denQ2,12000);
-setTimeout(denQ3,13000);
-setTimeout(denQ4,14000);
-setTimeout(detQ1,15000);
-setTimeout(detQ2,16000);
-setTimeout(detQ3,17000);
-setTimeout(detQ4,18000);
+setTimeout(denQ5,11000);
+setTimeout(detQ5,12000);
+setTimeout(denQ6,13000);
+setTimeout(detQ6,14000);
+setTimeout(denQ7,15000);
+setTimeout(detQ7,16000);
+setTimeout(denQ8,17000);
+setTimeout(detQ8,18000);
 }
 
 
@@ -61,22 +61,44 @@ function denQ1() {
     function denQ4() {
     text(numbers[3],430,240);
     }
+    function denQ5() {
+    text(numbers[4],210,240);
+    }
+    function denQ6() {
+    text(numbers[5],280,240);
+    }
+    function denQ7() {
+    text(numbers[6],355,240);
+    }
+    function denQ8() {
+    text(numbers[7],430,240);
+    }
+
     function detQ1() {
-text(numbers[4],210,290);
+text(numbers2[0],210,290);
     }
     function detQ2() {
-text(numbers[5],280,290);
+text(numbers2[1],280,290);
     }
     function detQ3() {
-text(numbers[6],355,290);
+text(numbers2[2],355,290);
     }
     function detQ4() {
-text(numbers[7],430,290);
+text(numbers2[3],430,290);
 
     }
 
-function denQ5() {
-text(numbers[7],430,290);
+ function detQ5() {
+text(numbers2[4],210,290);
+    }
+    function detQ6() {
+text(numbers2[5],280,290);
+    }
+    function detQ7() {
+text(numbers2[6],355,290);
+    }
+    function detQ8() {
+text(numbers2[7],430,290);
 
     }
 
